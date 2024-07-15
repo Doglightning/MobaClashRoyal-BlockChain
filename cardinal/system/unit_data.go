2fb80f07bf5169a61d2b957f87e7b2ea1ed4710b
doglightning <mrdoglightning@gmail.com>
2024-07-15 16:28:16 -0400

    updated archer ms

--- cardinal/system/unit_data.go
+++ cardinal/system/unit_data.go
@@ -16,8 +16,8 @@ type UnitType struct {
 }
 
 var UnitRegistry = map[string]UnitType{
-	"Vampire":    {Class: "melee", Health: 100, Damage: 1, AttackRate: 10, DamageFrame: 4, Speed: 50, Cost: 50, Radius: 80, AggroRadius: 1400, AttackRadius: 10},
-	"ArcherLady": {Class: "range", Health: 75, Damage: 35, AttackRate: 30, DamageFrame: 20, Speed: 0, Cost: 50, Radius: 50, AggroRadius: 1400, AttackRadius: 1200},
+	"Vampire":    {Class: "melee", Health: 100, Damage: 10, AttackRate: 10, DamageFrame: 4, Speed: 50, Cost: 50, Radius: 80, AggroRadius: 1400, AttackRadius: 10},
+	"ArcherLady": {Class: "range", Health: 75, Damage: 35, AttackRate: 30, DamageFrame: 20, Speed: 50, Cost: 50, Radius: 50, AggroRadius: 1400, AttackRadius: 1200},
 }
 
 type ProjectileType struct {
