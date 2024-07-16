fb15a084bad21aca5ef9249c24dd9af6d35cb6b9
doglightning <mrdoglightning@gmail.com>
2024-07-15 22:10:50 -0400

    minor documentaion update

--- cardinal/system/map_data.go
+++ cardinal/system/map_data.go
@@ -1,18 +1,22 @@
 package system
 
+// size of collision hash map. (should be atleast 1.5x the size of the largest unit)
 var SpatialGridCellSize = 300
 
 type MapData struct {
+	//Direction Map Data
 	StartX    int `json:"startX"`
 	StartY    int `json:"startY"`
 	EndX      int `json:"endX"`
 	EndY      int `json:"endY"`
 	Increment int `json:"increment"`
 
+	//Sturcture spawn points
 	Bases  [][]int `json:"bases"` //[0=Blue 1= red][x, y]
 	Towers [][]int `json:"towers"`
 }
 
+// Maps
 var MapDataRegistry = map[string]MapData{
 	"ProtoType": {StartX: -5440, StartY: -3660, EndX: 5260, EndY: 4640, Increment: 100, Bases: [][]int{{3860, 500}, {-3680, 700}}},
 }
@@ -22,6 +26,7 @@ type StructureData struct {
 	Radius int
 }
 
+// structures
 var StructureDataRegistry = map[string]StructureData{
 	"Base": {Health: 200, Radius: 240},
 }
