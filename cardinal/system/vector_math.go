848f9b542b7c627117c15e86899db0f825bbfcb4
doglightning <mrdoglightning@gmail.com>
2024-07-15 23:02:22 -0400

    added normalize function

--- cardinal/system/vector_math.go
+++ cardinal/system/vector_math.go
@@ -2,25 +2,25 @@ package system
 
 import "math"
 
-// func subtractVectorMath(x1, y1, x2, y2 float32) (x, y float32) {
+// func subtract(x1, y1, x2, y2 float32) (x, y float32) {
 // 	return x1 - x2, y1 - y2
 // }
 
-// func addVectorMath(x1, y1, x2, y2 float32) (x, y float32) {
+// func add(x1, y1, x2, y2 float32) (x, y float32) {
 // 	return x1 + x2, y1 + y2
 // }
 
-// func multiplyVectorMath(x, y float32, scalar float32) (float32, float32) {
+// func multiply(x, y float32, scalar float32) (float32, float32) {
 // 	return x * scalar, y * scalar
 // }
 
-// func normalizeVectorMath(x, y float32) (float32, float32) {
-// 	mag := math.Sqrt(float64(x*x + y*y))
-// 	if mag > 0 {
-// 		return x / float32(mag), y / float32(mag)
-// 	}
-// 	return 0, 0 // Return zero vector if magnitude is zero to prevent division by zero
-// }
+func normalize(x, y float32) (float32, float32) {
+	mag := math.Sqrt(float64(x*x + y*y))
+	if mag > 0 {
+		return x / float32(mag), y / float32(mag)
+	}
+	return 0, 0 // Return zero vector if magnitude is zero to prevent division by zero
+}
 
 func dotProduct(x1, y1, x2, y2 float32) float32 {
 	return float32(x1*x2 + y1*y2)
