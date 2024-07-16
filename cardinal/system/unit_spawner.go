92c415d271e522d0b12e952c0758d00115314ad7
doglightning <mrdoglightning@gmail.com>
2024-07-16 02:16:51 -0400

    updated spawn logic

--- cardinal/system/unit_spawner.go
+++ cardinal/system/unit_spawner.go
@@ -2,7 +2,6 @@ package system
 
 import (
 	"fmt"
-	"math"
 
 	"pkg.world.dev/world-engine/cardinal"
 	"pkg.world.dev/world-engine/cardinal/iterators"
@@ -13,61 +12,59 @@ import (
 )
 
 // Spawns player units
+// called from create_unit.go msg
 func UnitSpawnerSystem(world cardinal.WorldContext) error {
-	return cardinal.EachMessage[msg.CreateUnitMsg, msg.CreateUnitResult](
-		world,
+	return cardinal.EachMessage(world,
 		func(create cardinal.TxData[msg.CreateUnitMsg]) (msg.CreateUnitResult, error) {
 			//create filter for matching ID's
 			matchFilter := cardinal.ComponentFilter[comp.MatchId](func(m comp.MatchId) bool {
 				return m.MatchId == create.Msg.MatchID
 			})
-			gameStateSearch := cardinal.NewSearch().Entity(
+			//get game state
+			gameState, err := cardinal.NewSearch().Entity(
 				filter.Exact(GameStateFilters())).
-				Where(matchFilter)
-
-			gameState, err := gameStateSearch.First(world)
-
+				Where(matchFilter).First(world)
 			if err != nil {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error searching for match: %w", err)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error searching for match (unit_spawner.go): %w", err)
 			}
-
 			if gameState == iterators.BadID { // Assuming cardinal.NoEntity represents no result found
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("no match found with ID or missing components: %s", create.Msg.MatchID)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("no match found with ID or missing components (unit_spawner.go): %s", create.Msg.MatchID)
 			}
 
-			//check if unit being spawned exsists in the registry above
+			//check if unit being spawned exsists in the unit registry
 			unitType, ok := UnitRegistry[create.Msg.UnitType]
 			if !ok {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("unit type %s not found in registry", create.Msg.UnitType)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("unit type %s not found in registry (unit_spawner.go)", create.Msg.UnitType)
 			}
 
-			//get UID
-			tempUID, err := cardinal.GetComponent[comp.UID](world, gameState)
+			//get new UID
+			UID, err := getNextUID(world, create.Msg.MatchID)
 			if err != nil {
-				return msg.CreateUnitResult{}, fmt.Errorf("error getting UID: %w", err)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("(unit_spawner.go) - %w", err)
 			}
 
 			//calculate distance from enemy spawn
 			var tempDistance float32
 			if create.Msg.Team == "Blue" {
-				tempDistance = float32(math.Sqrt(((float64(create.Msg.PositionX) - float64(MapDataRegistry[create.Msg.MapName].Bases[1][0])) * (float64(create.Msg.PositionX) - float64(MapDataRegistry[create.Msg.MapName].Bases[1][0]))) + ((float64(create.Msg.PositionY) - float64(MapDataRegistry[create.Msg.MapName].Bases[1][1])) * (float64(create.Msg.PositionY) - float64(MapDataRegistry[create.Msg.MapName].Bases[1][1])))))
+				tempDistance = float32(distanceBetweenTwoPoints(float64(MapDataRegistry[create.Msg.MapName].Bases[1][0]), float64(MapDataRegistry[create.Msg.MapName].Bases[1][1]), float64(create.Msg.PositionX), float64(create.Msg.PositionY)))
 			} else {
-				tempDistance = float32(math.Sqrt(((float64(create.Msg.PositionX) - float64(MapDataRegistry[create.Msg.MapName].Bases[0][0])) * (float64(create.Msg.PositionX) - float64(MapDataRegistry[create.Msg.MapName].Bases[0][0]))) + ((float64(create.Msg.PositionY) - float64(MapDataRegistry[create.Msg.MapName].Bases[0][1])) * (float64(create.Msg.PositionY) - float64(MapDataRegistry[create.Msg.MapName].Bases[0][1])))))
+				tempDistance = float32(distanceBetweenTwoPoints(float64(MapDataRegistry[create.Msg.MapName].Bases[0][0]), float64(MapDataRegistry[create.Msg.MapName].Bases[0][1]), float64(create.Msg.PositionX), float64(create.Msg.PositionY)))
 			}
 
-			//get SpatialHash component from game state
+			//get collision Hash component from game state
 			SpatialHash, err := cardinal.GetComponent[comp.SpatialHash](world, gameState)
 			if err != nil {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error getting SpatialHash component (unit Spawner): %w", err)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error getting SpatialHash component (unit_spawner.go): %w", err)
 			}
+			//check if spawning on a taken spot in collision hash
 			if CheckCollisionSpatialHash(SpatialHash, create.Msg.PositionX, create.Msg.PositionY, unitType.Radius) {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("collision with unit")
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("collision with unit (unit_spawner.go)")
 			}
 
 			//create unit
 			entityID, errr := cardinal.Create(world,
 				comp.MatchId{MatchId: create.Msg.MatchID},
-				comp.UID{UID: tempUID.UID},
+				comp.UID{UID: UID},
 				comp.UnitName{UnitName: create.Msg.UnitType},
 				comp.Team{Team: create.Msg.Team},
 				comp.Health{CurrentHP: unitType.Health, MaxHP: unitType.Health},
@@ -79,16 +76,10 @@ func UnitSpawnerSystem(world cardinal.WorldContext) error {
 				comp.Attack{Combat: false, Damage: unitType.Damage, Rate: unitType.AttackRate, Frame: 0, DamageFrame: unitType.DamageFrame, Class: unitType.Class, AttackRadius: unitType.AttackRadius, AggroRadius: unitType.AggroRadius},
 			)
 			if errr != nil {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error creating unit: %w", err)
+				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error creating unit (unit_spawner.go): %w", err)
 			}
-			//add unit to spatial hash collision map
+			//add unit to collision hash collision map
 			AddObjectSpatialHash(SpatialHash, entityID, create.Msg.PositionX, create.Msg.PositionY, unitType.Radius, create.Msg.Team)
-
-			//incriment UID
-			tempUID.UID++
-			if err := cardinal.SetComponent[comp.UID](world, gameState, tempUID); err != nil {
-				return msg.CreateUnitResult{Success: false}, fmt.Errorf("error updating UID: %w", err)
-			}
 
 			return msg.CreateUnitResult{Success: true}, nil
 		})
